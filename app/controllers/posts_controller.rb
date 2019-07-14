06aff7a319f534a1ba55caf997be01d9b29a2ae5
DoroYoshi <yoshimatsu44dy@gmail.com>
2019-07-14 10:50:42 +0900

    postの登録機能 照会機能 編集機能の作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,41 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post, notice: "ブログを登録しました。"
+    else
+      render :new #@postの内容そのままにnew.html.erbの内容を表示する。
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post, notice: "ブログを更新しました。"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
